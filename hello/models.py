89cabc010d053d7e8e6f01a29c843611a67524c1
Deepika_bala <deepikabala07@gmail.com>
2021-06-29 20:11:56 +0530

    push

--- hello/models.py
+++ hello/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Request(models.Model):
+    req_id = models.AutoField(db_column='req_id', primary_key=True)
+    f_name = models.CharField(max_length=30, null=False)
+    l_name = models.CharField(max_length=30, null=False)
+    user_dob = models.CharField(max_length=20,null=False)
+    user_gender = models.CharField(max_length=30, null=False)
+    user_nationality = models.CharField(max_length=30, null=False)
+    user_city = models.CharField(max_length=30, null=False)
+    user_state = models.CharField(max_length=30, null=False)
+    user_pincode =models.CharField(max_length=6, null=False)
+    user_qualification =models.CharField(max_length=30, null=False)
+    user_salary = models.IntegerField(null=False)
+    user_pannumber = models.CharField(max_length=10, null=False)
+
--- hello/views.py
+++ hello/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import datetime
-
+from hello.models import Request
 # Create your views here.
 def home(request):
     return render(request,"home.html")
@@ -32,6 +32,8 @@ def display(request):
                                     if len(qualification)>2:
                                         if salary >10000:
                                             if len(pannumber) == 10:
+                                                b=Request(req_id=1,f_name=firstname,l_name=lastname,user_dob=dob,user_gender=gender,user_nationality=nationality,user_city=city,user_state=state,user_pincode=pincode,user_qualification=qualification,user_salary=salary,user_pannumber=pannumber)
+                                                b.save()
                                                 return render(request,"result.html",{'operation': 'success'})
                                             else:
                                                 return render(request,'result.html',{'operation':'reject'})
